ecb72a27255e1ff8c8f2df9354add2f1c7b01685
JustinR <jpmremij@avans.nl>
2018-05-25 11:03:06 +0200

    merge fix
    merge fix

--- App/Models/Product.php
+++ App/Models/Product.php
@@ -11,27 +11,13 @@ use PDO;
  */
 class Product extends \Core\Model
 {
-<<<<<<< HEAD
+
     private $productId;
     private $productName;
     private $productDesc;
     private $salesPrice;
-=======
-    public $id;
-    public $productName;
-    public $productDesc;
-    public $salesPrice;
-    public $imgUrl;
+    private $imgUrl;
 
-    public function __construct($productId, $productName, $productDesc, $salesPrice, $imgUrl)
-    {
-        $this->id = $productId;
-        $this->productName = $productName;
-        $this->productDesc = $productDesc;
-        $this->salesPrice = $salesPrice;
-        $this->imgUrl = $imgUrl;
-    }
->>>>>>> 63620cf3b8234c3878eb1217922ff87f29ae4f0d
 
     public static function constructFromDatabase($id)
     {
@@ -40,7 +26,6 @@ class Product extends \Core\Model
         $stmt = $db->prepare('select `productId`, `productName`, `productDesc`, `salesPrice`, `imgUrl` from `product` WHERE `productId` = :id');
         $stmt->bindParam(':id', $id);
         $stmt->execute();
-<<<<<<< HEAD
         return $stmt->fetchObject('App\Models\Product');
     }
 
@@ -62,9 +47,9 @@ class Product extends \Core\Model
     public function getSalesPrice()
     {
         return $this->salesPrice;
-=======
-        return $stmt->fetchObject('App\Models\Product' ,['productId', 'productName', 'productDesc', 'salesPrice', 'imgUrl']);
->>>>>>> 63620cf3b8234c3878eb1217922ff87f29ae4f0d
     }
-
+    public function getImgUrl()
+    {
+        return $this->imgUrl;
+    }
 }
